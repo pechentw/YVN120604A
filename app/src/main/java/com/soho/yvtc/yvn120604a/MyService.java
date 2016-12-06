6bc9f5cd719b726777b0a3367ffe58ac8858517f
pechentw <pechen.tw@gmail.com>
2016-12-06 21:22:50 +0800

    Initial commit

---
+++ app/src/main/java/com/soho/yvtc/yvn120604a/MyService.java
@@ -0,0 +1,23 @@
+package com.soho.yvtc.yvn120604a;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("SERVICE","This is on start command");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
